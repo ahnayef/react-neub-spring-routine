a4ec0a8e2b06bb81d9c158c03c6fb6287c780ca6
AHNayef <ahndo7059@gmail.com>
2023-01-31 18:59:03 +0600

    Home complete

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './home.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className="homeMain">
-      <h1>NEUB | North East University Bangladesh</h1>
+      <h1>North East University Bangladesh</h1>
       <h2>Spring 23 Routine</h2>
+      <div className="homeSelectArea">
+      <h3>Select your Section</h3>
+      <Link to="/"><button className='btn'>Section A</button></Link>
+      <Link to="/"><button className='btn'>Section B</button></Link>
+      </div>
     </div>
   )
 }
