280b95423ecf1cdb89cdd7438e880de132dbbc5b
AHN <89353441+ahnayef@users.noreply.github.com>
2023-02-14 06:32:10 +0600

    Added 2 react-icons

--- src/Components/Routine/RoutineBox/Routinebox.jsx
+++ src/Components/Routine/RoutineBox/Routinebox.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import "./routinebox.css"
+import "./routinebox.css";
+import {FaClock} from 'react-icons/fa';
+import {MdLocationOn} from 'react-icons/md';
 
 export default function Routinebox(props) {
     return (
@@ -7,8 +9,8 @@ export default function Routinebox(props) {
             {props.lab ? <div className="lab">LAB</div> : " "}
             <h2 className='gre-text'>{props.name}</h2>
             <h4>{props.teacher}</h4>
-            <h3>{props.startTime} - {props.endTime}</h3>
-            <h3>{props.roomNo}</h3>
+            <h3><FaClock/> {props.startTime} - {props.endTime}</h3>
+            <h3><MdLocationOn/> {props.roomNo}</h3>
             {props.section && <h3>{props.section} only</h3>}
         </div>
     )
