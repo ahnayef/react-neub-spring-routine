4a99aff27d8bb11a6ff58c1d30b277e5098be21a
AHN <89353441+ahnayef@users.noreply.github.com>
2023-02-14 06:35:23 +0600

    Added <i> tag on react-icons

--- src/Components/Routine/RoutineBox/Routinebox.jsx
+++ src/Components/Routine/RoutineBox/Routinebox.jsx
@@ -9,8 +9,8 @@ export default function Routinebox(props) {
             {props.lab ? <div className="lab">LAB</div> : " "}
             <h2 className='gre-text'>{props.name}</h2>
             <h4>{props.teacher}</h4>
-            <h3><FaClock/> {props.startTime} - {props.endTime}</h3>
-            <h3><MdLocationOn/> {props.roomNo}</h3>
+            <h3><i><FaClock/></i> {props.startTime} - {props.endTime}</h3>
+            <h3><i><MdLocationOn/></i> {props.roomNo}</h3>
             {props.section && <h3>{props.section} only</h3>}
         </div>
     )
