47a7d823e5360a754851577768fc299c02abf53a
AHNayef <ahnayef@outlook.com>
2023-07-07 21:20:50 +0600

    Back to hashrouting

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import Error404 from './Components/Error404';
@@ -9,22 +9,22 @@ import Routine from "./Components/Routine/Routine"
 import Cr from "./Components/Cr/Cr"
 import './index.css';
 
-const router =  createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
-    element: <Routine/>,
-    errorElement:<Error404/>
-  },{
+    element: <Routine />,
+    errorElement: <Error404 />
+  }, {
     path: "/cr",
-    element: <Cr/>,
-    errorElement:<Error404/>
+    element: <Cr />,
+    errorElement: <Error404 />
   }
-  
+
 ]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
