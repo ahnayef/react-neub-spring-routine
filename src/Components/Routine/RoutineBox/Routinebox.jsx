d4d0235e915f66c3f33ad26cfb7afdfda2a3cb78
AHNayef <ahnayef@outlook.com>
2023-07-25 22:16:54 +0600

    Fix object issue in GA

--- src/Components/Routine/RoutineBox/Routinebox.jsx
+++ src/Components/Routine/RoutineBox/Routinebox.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./routinebox.css";
-import {FaClock} from 'react-icons/fa';
-import {MdLocationOn} from 'react-icons/md';
-import {AiOutlineAlert} from 'react-icons/ai';
+import { FaClock } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
+import { AiOutlineAlert } from 'react-icons/ai';
 import { useEffect } from 'react';
 import ReactGA from "react-ga4"
 
@@ -11,28 +11,30 @@ export default function Routinebox(routine) {
 
     // console.log(time);
 
-    const { courseCode, lab, name, teacher, time, roomNo, section} = routine;
+    const { courseCode, lab, name, teacher, time, roomNo, section } = routine;
 
 
-    useEffect(()=>{
-    
-        ReactGA.event({
-            category:"Subject View",
-            action:{name},
-            label:`View ${name} subject`
-        })
-        ReactGA.event({
-            category:"Room",
-            action:{roomNo},
-            label:`Visit ${roomNo}`
-        })
-        ReactGA.event({
-            category:"Teacher",
-            action:{teacher},
-            label:`Class taken by ${teacher}`
-        })
+    useEffect(() => {
 
-    },[]);
+        ReactGA.event(
+            {
+                category: "Subject View",
+                action: { name },
+                label: `View ${name} subject`
+            },
+            {
+                category: "Room",
+                action: { roomNo },
+                label: `Visit ${roomNo}`
+            },
+            {
+                category: "Teacher",
+                action: { teacher },
+                label: `Class taken by ${teacher}`
+            }
+        )
+
+    }, []);
 
     return (
         <div className="routineBox">
@@ -40,13 +42,15 @@ export default function Routinebox(routine) {
             {lab ? <div className="lab">LAB</div> : " "}
             <h2 className='gre-text'>{name}</h2>
             <h4>{teacher}</h4>
-            <div className="time">                
-            {time.map((time,index)=>{return(
-            <h3 key={index}><i><FaClock/></i> {time}</h3>
-            )})}
+            <div className="time">
+                {time.map((time, index) => {
+                    return (
+                        <h3 key={index}><i><FaClock /></i> {time}</h3>
+                    )
+                })}
             </div>
-            <h3><i><MdLocationOn/></i> {roomNo}</h3>
-            {section && <h3><i className='alertSec'><AiOutlineAlert/></i> {section} <b>only</b></h3>}
+            <h3><i><MdLocationOn /></i> {roomNo}</h3>
+            {section && <h3><i className='alertSec'><AiOutlineAlert /></i> {section} <b>only</b></h3>}
         </div>
     )
 }
