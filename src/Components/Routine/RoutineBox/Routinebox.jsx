9ff9b57ba0477bbfcc0a95165e39307524153af0
AHNayef <ahnayef@outlook.com>
2025-04-07 22:23:28 +0600

    Remove unused star element from Routinebox component

--- src/Components/Routine/RoutineBox/Routinebox.jsx
+++ src/Components/Routine/RoutineBox/Routinebox.jsx
@@ -65,7 +65,6 @@ export default function Routinebox(routine) {
                     <span className="star"></span>
                     <span className="star"></span>
                     <span className="star"></span>
-                    <span className="star"></span>
                 </div>
 
             </div>
